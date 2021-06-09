6c1d8f06fb54711fe72d879edbd3f3431fb6ca8d
Dasha <dasha.kotova.mail@gmail.com>
2021-06-09 17:14:22 -0400

    add basic route components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import DetailsPage from "./routes/DetailsPage";
+import HomePage from "./routes/HomePage";
+import UpdatePage from "./routes/UpdatePage";
 
 const App = () => {
   return (
-    
+  <div>
+
+    <Router>
+      <Switch></Switch>
+      <Route exact path="/" component={HomePage}/>
+      <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
+      <Route exact path="/restaurants/:id" component={DetailsPage}/>
+    </Router>
+
+  </div>
   )
 
-}
+};
 
 export default App;
